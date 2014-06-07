b19486fe70096ff55b8d1af19f7b217da00170b9
Jeckel <jeckel@jeckel.fr>
2014-06-07 14:43:56 +0200

    Test RGB Led with Color library

--- Temple.ino
+++ Temple.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Color.h>
 
+/**
+ * RGB Led test plugged on PWM output of Arduino Mega 2560
+ */
+#define LED_RED    44
+#define LED_GREEN  45
+#define LED_BLUE   46
+
+/**
+ * Init
+ */
+void setup()
+{
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly: 
-  
+/**
+ * Main loop
+ */
+void loop()
+{
+    // Init color structure
+    struct CRGB c;
+    for (uint16_t i = 0; i < 384; i+= 1)
+    {
+        // Get CRGB value from index in an RGB wheel
+        c.Wheel(i);
+        // Output color
+        analogWrite(LED_RED, 255 - c.r * 2);
+        analogWrite(LED_BLUE, 255 - c.b * 2);
+        analogWrite(LED_GREEN, 255 - c.g * 2);
+        // Delay
+        delay(25);
+    }
 }
