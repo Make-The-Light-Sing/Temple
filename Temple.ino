ce7ea214aaee5135ee04b583d7cfb31193c79c2e
Jeckel <jeckel@jeckel.fr>
2014-06-07 15:10:57 +0200

    Add HC-SR04 Sensor to define color according by distance read from the sensor

--- Temple.ino
+++ Temple.ino
@@ -1,3 +1,4 @@
+#include <HCSR04UltraSonic.h>
 #include <Color.h>
 
 /**
@@ -7,6 +8,13 @@
 #define LED_GREEN  45
 #define LED_BLUE   46
 
+#define TRIGGER_PIN 9
+#define ECHO_PIN    8
+
+//#define DEBUG
+
+HCSR04UltraSonic HCSR04(TRIGGER_PIN, ECHO_PIN);
+
 /**
  * Init
  */
@@ -15,6 +23,10 @@ void setup()
     pinMode(LED_RED, OUTPUT);
     pinMode(LED_GREEN, OUTPUT);
     pinMode(LED_BLUE, OUTPUT);
+    
+    #ifdef DEBUG
+        Serial.begin(9600);
+    #endif
 }
 
 /**
@@ -24,15 +36,19 @@ void loop()
 {
     // Init color structure
     struct CRGB c;
-    for (uint16_t i = 0; i < 384; i+= 1)
-    {
-        // Get CRGB value from index in an RGB wheel
-        c.Wheel(i);
-        // Output color
-        analogWrite(LED_RED, 255 - c.r * 2);
-        analogWrite(LED_BLUE, 255 - c.b * 2);
-        analogWrite(LED_GREEN, 255 - c.g * 2);
-        // Delay
-        delay(25);
-    }
+    
+    // Read distance from Sensor
+    int distance = HCSR04.readDistance();
+    #ifdef DEBUG
+        Serial.println(distance);
+    #endif
+        
+    // Get CRGB value from index in an RGB wheel, index is the distance
+    c.Wheel(distance % 384);
+    // Output color
+    analogWrite(LED_RED, 255 - c.r * 2);
+    analogWrite(LED_BLUE, 255 - c.b * 2);
+    analogWrite(LED_GREEN, 255 - c.g * 2);
+    // Delay
+    delay(100);
 }
