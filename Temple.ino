0f1716e0f6d3233c47b59b532a88a3d27cd943f1
Jeckel <jeckel@jeckel.fr>
2014-06-07 15:57:24 +0200

    Use PIR Sensor, back and front to turn the led ON or OFF according to movement detection

--- Temple.ino
+++ Temple.ino
@@ -1,3 +1,4 @@
+#include <PIRSensor.h>
 #include <HCSR04UltraSonic.h>
 #include <Color.h>
 
@@ -11,9 +12,14 @@
 #define TRIGGER_PIN 9
 #define ECHO_PIN    8
 
-//#define DEBUG
+#define PIR_FRONT_PIN 3
+#define PIR_BACK_PIN  2
+
+#define DEBUG
 
 HCSR04UltraSonic HCSR04(TRIGGER_PIN, ECHO_PIN);
+PIRSensor        PIRFront(PIR_FRONT_PIN);
+PIRSensor        PIRBack(PIR_BACK_PIN);
 
 /**
  * Init
@@ -26,7 +32,7 @@ void setup()
     
     #ifdef DEBUG
         Serial.begin(9600);
-    #endif
+    #endif   
 }
 
 /**
@@ -36,19 +42,31 @@ void loop()
 {
     // Init color structure
     struct CRGB c;
-    
+
     // Read distance from Sensor
     int distance = HCSR04.readDistance();
     #ifdef DEBUG
         Serial.println(distance);
+        if (PIRFront.hasMovement()) {
+            Serial.println("Movement in front sensor");
+        }
+        if (PIRBack.hasMovement()) {
+            Serial.println("Movement in back sensor");
+        }
     #endif
         
     // Get CRGB value from index in an RGB wheel, index is the distance
     c.Wheel(distance % 384);
     // Output color
-    analogWrite(LED_RED, 255 - c.r * 2);
-    analogWrite(LED_BLUE, 255 - c.b * 2);
-    analogWrite(LED_GREEN, 255 - c.g * 2);
+    if (PIRFront.hasMovement() || PIRBack.hasMovement()) {
+        analogWrite(LED_RED, 255 - c.r * 2);
+        analogWrite(LED_BLUE, 255 - c.b * 2);
+        analogWrite(LED_GREEN, 255 - c.g * 2);
+    } else {
+        analogWrite(LED_RED, 255);
+        analogWrite(LED_BLUE, 255);
+        analogWrite(LED_GREEN, 255);
+    }
     // Delay
     delay(100);
 }
